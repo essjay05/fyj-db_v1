73c01b9b4c8c0be41ef8f93130bc47cfec11ee4f
Joy Serquiña <stepjoyous@gmail.com>
2023-02-02 17:03:12 -0800

    Installed handlebars and pug. Added layouts for main-layout.pug, created home.pug and user.pug.

--- server.js
+++ server.js
@@ -4,18 +4,22 @@ const PORT = process.env.PORT || 8888
 
 const app = express()
 
+// Middleware
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
 // Routes
-// app.get('/', (req, res, next) => {
-//   res.render()
-// })
+app.get('/', (req, res, next) => {
+  res.render()
+})
 
-// app.get('/users', (req, res, next) => {
-//   res.render()
-// })
+app.get('/users', (req, res, next) => {
+  res.render()
+})
 
-// app.post('/add-user', (req, res, next) => {
-//   res.redirect('/users')
-// })
+app.post('/add-user', (req, res, next) => {
+  res.redirect('/users')
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening on PORT:${PORT}`)
